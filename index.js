a9e6530f00718998aff30f4506b962708d824455
Nick <nikheal25@gmail.com>
2020-12-06 08:14:59 +1100

    worlkign

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+import schema from "./schema";
+import { graphqlHTTP } from "express-graphql";
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+const root = { hello: () => "Hi, My name is Manny" };
+
+app.use(
+  "/graphql",
+  graphqlHTTP({ schema: schema, rootValue: root, graphiql: true })
+);
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
